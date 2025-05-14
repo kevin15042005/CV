154478edf01763a6a0ddd14692466d63cb278909
Kevin Alejandro Reyes Sarmeinto <kevin2013118@gmail.com>
2025-05-14 14:25:31 -0500

    Se hace el cambio en vite nuevamente y se cambio el package json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port:process.env || 5173
-  }
+  plugins: [react()],
+  base: './', 
 })
