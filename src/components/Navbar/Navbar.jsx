7d481c03e62f97c4d665890a8b5ae08f15965cb2
Kevin Alejandro Reyes Sarmeinto <kevin2013118@gmail.com>
2025-05-14 14:44:13 -0500

    Modificacion en vite

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
         <div className="Item-Principal">
           <ul className={ click ? "nav-menu active":"nav-menu"}>
             <li><Link to="Principal" smooth={true} duration={800} onClick={closeMenu}>Inicio</Link></li>
+            <li><Link to="sobre-mi" smooth={true} duration={800} onClick={closeMenu}>Sobre MI</Link></li>
             <li><Link to="Habilidades" smooth={true} duration={800} onClick={closeMenu}>Habilidades</Link></li>
             <li><Link to="Proyecto" smooth={true} duration={800} onClick={closeMenu}>Proyectos</Link></li>
             <li><Link to="Contacto" smooth={true} duration={800} onClick={closeMenu}>Contacto</Link> </li>           
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   preview: {
-    allowedHosts: ['cv-rmph.onrender.com']
-  }
+    allowedHosts: ['cv-rmph.onrender.com'],
+  },
+  server: {
+    port: process.env.PORT || 10000,  // Usar el puerto proporcionado por Render
+    host: '0.0.0.0',  // Permitir acceso desde cualquier red
+  },
 })
